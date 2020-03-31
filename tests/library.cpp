682acb582edc77829f2d3e4bcadcde02cf68f12b
himself65 <himself65@outlook.com>
2020-03-31 14:47:59 +0800

    test: update

--- tests/library.cpp
+++ tests/library.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include <iostream>
+#include <vector>
 #include "library.h"
 
 TEST(sample_test_case, sample_test) {
@@ -12,4 +12,12 @@ TEST(sample_test_case, hellp_test) {
 
 TEST(sprintf_test_case, base_test) {
   EXPECT_EQ(SPrintF("foo"), "foo");
+}
+
+TEST(vector_test_case, base_test) {
+  std::vector<int> my_vector;
+  my_vector.push_back(1);
+  my_vector.push_back(2);
+  my_vector.push_back(3);
+  EXPECT_EQ(my_vector.size(), 3);
 }
